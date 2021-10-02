b1466535916adab44204f978e02b13a9efba15a1
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-10-02 13:52:32 -0500

    estilos navbar

---
+++ gentefront/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "media/logo.png";
+import { useState } from "react";
+
+//hook de usestate para activar hamburguer
+
+export const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <>
+      {/* navbar */}
+
+      <nav className="flex items-center flex-wrap bg-gray-800 p-3 ">
+        <Link href="/">
+          {/* logo*/}
+          <a className="inline-flex items-center p-1 mr-4 ">
+            <Image src={logo} alt="logo" />
+            <span className="text-xl text-white font-bold uppercase tracking-wide">
+              Gente Prevalente
+            </span>
+          </a>
+        </Link>
+        {/* hamburguer*/}
+        <button
+          className=" inline-flex p-3  hover:bg-yellow-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none
+        "
+          onClick={handleClick}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+
+        {/* vista de ordenador */}
+
+        <div
+          className={`${
+            active ? "" : "hidden"
+          }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+        >
+          <div className="lg:inline-flex lg:flex-row lg:inset-10  w-full lg:items-center items-start  flex flex-col lg:h-auto justify-between">
+            <Link href="/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3   py-2 rounded text-white text-2xl  items-center justify-center hover:bg-yellow-600 hover:text-white ">
+                <i class="fas fa-search mr-4"></i>Buscar
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-2xl  items-center justify-center hover:bg-yellow-600 hover:text-white">
+                <i class="fas fa-cogs mr-4"></i>Administracion
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-2xl  items-center justify-center hover:bg-yellow-600 hover:text-white">
+                <i class="fas fa-suitcase mr-4"></i>Empleo
+                <i class="fas fa-chevron-down ml-4"></i>
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-2xl  items-center justify-center hover:bg-yellow-600 hover:text-white">
+                <i class="fas fa-clipboard-list mr-4"></i> Mi CV
+              </a>
+            </Link>
+            <Link href="/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-2xl items-center justify-center hover:bg-yellow-600 hover:text-white">
+                <i class="fas fa-circle mr-4"></i> Usuario
+                <i class="fas fa-chevron-down ml-4"></i>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
--- gentefront/pages/index.js
+++ gentefront/pages/index.js
@@ -1,12 +1,15 @@
-import Head from 'next/head';
+import Head from "next/head";
+import { Navbar } from "components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>Gente Prevalente</title>
+        <link rel="icon" href="/favicon.ico" />
+        <script src="https://kit.fontawesome.com/0888d5f4dd.js" crossorigin="anonymous"></script>
       </Head>
+      <Navbar />
       <div>Hello World</div>
     </div>
   );
