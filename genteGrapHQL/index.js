4075e3c5a5e55909071c97952494e5e0ce3b3b6b
Judmontoyaso <juandavidsolorzano73@gmail.com>
2021-10-04 09:53:32 -0500

    conectar base de datos usando mongoose como ORM

--- genteGrapHQL/index.js
+++ genteGrapHQL/index.js
@@ -1,7 +1,10 @@
 const {ApolloServer} = require('apollo-server');
-const typeDefs = require('./db/schema')
-const resolvers = require('./db/resolvers')
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+const conectarDB = require('./config/db');
 
+//Conectar a la base de datos
+conectarDB();
 
 //servidor
 const server = new ApolloServer({
